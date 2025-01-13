48dba4f60e1256a598d9fb5aa7f4055892e7baee
Silent-Suspect <Sascha.Mueller@rub.de>
2025-01-13 09:59:29 +0100

    Working Static Website Generator

--- src/main.py
+++ src/main.py
@@ -1,11 +1,26 @@
-from textnode import *
-from htmlnode import *
+import os
+import shutil
+
+from copystatic import copy_files_recursive
+from gencontent import generate_pages_recursive
+
+
+dir_path_static = "./static"
+dir_path_public = "./public"
+dir_path_content = "./content"
+template_path = "./template.html"
+
 
 def main():
-    #node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
-    node0 = HTMLNode()
-    node = HTMLNode("<a>", "hey hoe", node0, {"href": "https://www.google.com"})
-    #print(node)
-    #print(node.props_to_html())
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
+
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
+
+    print("Generating content...")
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
+
 
 main()
