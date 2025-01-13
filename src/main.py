82845b187bc039b3177d6b942d6570180c623ab6
Silent-Suspect <Sascha.Mueller@rub.de>
2025-01-13 07:56:09 +0100

    Nodes

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
 from textnode import *
+from htmlnode import *
 
 def main():
-    
-    obj = TextNode("Beispieltext steht hier", TextType.NORMAL, "https://www.boot.dev")
-    print(obj)
+    #node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
+    node0 = HTMLNode()
+    node = HTMLNode("<a>", "hey hoe", node0, {"href": "https://www.google.com"})
+    #print(node)
+    #print(node.props_to_html())
 
 main()
